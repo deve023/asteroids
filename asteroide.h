905f73d819dafcaf1391adda90d680bd3fa00295
aflouret <aflouret@gmail.com>
2019-06-22 01:39:22 -0300

    agrego funciones que faltan a asteroide.h

--- asteroide.h
+++ asteroide.h
@@ -12,3 +12,6 @@ void asteroide_destruir(asteroide_t * a);
 void asteroide_mover(asteroide_t * a, float dt);
 bool asteroide_dibujar(const asteroide_t *a);
 bool asteroide_colision(const asteroide_t *a, float x, float y);
+float asteroide_get_x(const asteroide_t *a);
+float asteroide_get_y(const asteroide_t *a);
+float asteroide_get_radio(const asteroide_t *a);
