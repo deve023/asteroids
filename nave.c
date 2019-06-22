e3d786ef9a8540a014bcde5569579ae4140dbabd
deve023 <adevedia@fi.uba.ar>
2019-06-21 22:45:41 -0300

    Cambie el struct nave, de float posicion[2] a float x e float y, idem con la velocidad. Tambien implemente la funcion nave_get_angulo.

--- nave.c
+++ nave.c
@@ -1,8 +1,10 @@
 #include "nave.h"
 
 struct nave {
-    float posicion[2];
-    float velocidad[2];
+    float x;
+    float y;
+    float vx;
+    float vy;
     float angulo;
     float potencia;
 };
@@ -12,11 +14,11 @@ nave_t *nave_crear() {
     if(n == NULL)
         return NULL;
 
-    (n->posicion)[0] = NAVE_X_INICIAL;
-    (n->posicion)[1] = NAVE_Y_INICIAL;
+    n->x = NAVE_X_INICIAL;
+    n->y = NAVE_Y_INICIAL;
 
-    (n->velocidad)[0] = NAVE_VX_INICIAL;
-    (n->velocidad)[1] = NAVE_VY_INICIAL;
+    n->vx = NAVE_VX_INICIAL;
+    n->vy = NAVE_VY_INICIAL;
 
     n->angulo = NAVE_ANGULO_INICIAL;
     n->potencia = NAVE_POTENCIA_INICIAL;
@@ -28,11 +30,15 @@ void nave_destruir(nave_t *n) {
     free(n);
 }
 
-float nave_get_pos_x(const nave_t *n) {
-    return (n->posicion)[0];
+float nave_get_x(const nave_t *n) {
+    return n->x;
 }
-float nave_get_pos_y(const nave_t *n) {
-    return (n->posicion)[1];
+float nave_get_y(const nave_t *n) {
+    return n->y;
+}
+
+float nave_get_angulo(const nave_t *n) {
+    return n->angulo;
 }
 
 void nave_incrementar_angulo(nave_t *n, float angulo) {
@@ -45,19 +51,19 @@ void nave_impulso(nave_t *n) {
 
 void nave_mover(nave_t *n, float dt) {
 
-    (n->velocidad)[0] += n->potencia * cos(n->angulo) * dt;
-    (n->velocidad)[1] -= n->potencia * sin(n->angulo) * dt;
+    n->vx += n->potencia * cos(n->angulo) * dt;
+    n->vy -= n->potencia * sin(n->angulo) * dt;
 
-    (n->velocidad)[0] -= (n->velocidad)[0] * NAVE_VELOCIDAD_DECREC;
-    (n->velocidad)[1] -= (n->velocidad)[1] * NAVE_VELOCIDAD_DECREC;
+    n->vx -= n->vx * NAVE_VELOCIDAD_DECREC;
+    n->vy -= n->vy * NAVE_VELOCIDAD_DECREC;
 
-    (n->posicion)[0] += (n->velocidad)[0] * dt;
-    (n->posicion)[1] += (n->velocidad)[1] * dt;
-    graficador_ajustar_variables(n->posicion, n->posicion + 1);
+    n->x += n->vx * dt;
+    n->y += n->vy * dt;
+    graficador_ajustar_variables(&(n->x), &(n->y));
 
     n->potencia *= (1 - NAVE_POTENCIA_DECREC);
 }
 
 bool nave_dibujar(const nave_t *n) {
-    return graficador_dibujar(NAVE_SPRITE, ESCALA_NAVE, nave_get_pos_x(n), nave_get_pos_y(n), n->angulo) && graficador_dibujar(CHORRO_SPRITE, ESCALA_NAVE, nave_get_pos_x(n), nave_get_pos_y(n), n->angulo);
+    return graficador_dibujar(NAVE_SPRITE, ESCALA_NAVE, n->x, n->y, n->angulo) && graficador_dibujar(CHORRO_SPRITE, ESCALA_NAVE, n->x, n->y, n->angulo);
 }
